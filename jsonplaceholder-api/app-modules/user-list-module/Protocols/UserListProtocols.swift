daa718c8408fbab4c347439153672652932158bc
Leonardo Martes <>
2022-02-17 21:42:58 -0500

    Delegates and protocols are added.

--- jsonplaceholder-api/app-modules/user-list-module/Protocols/UserListProtocols.swift
+++ jsonplaceholder-api/app-modules/user-list-module/Protocols/UserListProtocols.swift
@@ -2,30 +2,29 @@ import Foundation
 import UIKit
 
 protocol ViewToPresenterProtocol {
-    var view: PresenterToViewProtocol { get set }
-    var interactor: PresenterToInteractorProtocol { get set }
-    var router: PresenterToRouterProtocol { get set }
+    var view: PresenterToViewProtocol? { get set }
+    var interactor: PresenterToInteractorProtocol? { get set }
+    var router: PresenterToRouterProtocol? { get set }
     func startFetchingUserList()
     func showUserPostView(with user: UserEntity, from view: UIViewController)
 }
 
 protocol PresenterToViewProtocol {
-    func showUserList(users: [UserEntity])
-    func showError()
+    func showUserList(with users: [UserEntity])
+    func showError(_ error: Error)
 }
 
 protocol PresenterToRouterProtocol {
-    //static func createUserPostModule(userListReference: UserListViewController)
-    static func createUserListModule() -> UserListViewController
-    func pushToUserPost(posts: [String], from view: UIViewController)
+    static func createUserListModule(userListReference: UserListViewController)
+    func pushToUserPost(with user: UserEntity, from view: UIViewController)
 }
 
 protocol PresenterToInteractorProtocol {
-    var presenter: InteractorToPresenterProtocol { get set }
+    var presenter: InteractorToPresenterProtocol? { get set }
     func fetchUserList()
 }
 
 protocol InteractorToPresenterProtocol {
     func userListFetchedSuccess(userList: [UserEntity])
-    func userListFetchFailed()
+    func userListFetchFailed(with error: Error)
 }
