fef0265eea1434e513797556b5597c3c8de00d8b
Leonardo Martes <>
2022-02-17 21:42:09 -0500

    Some constants are added.

--- common-utils/AppConstants.swift
+++ common-utils/AppConstants.swift
@@ -3,6 +3,11 @@ import Foundation
 let API_USER_LIST: String = "https://jsonplaceholder.typicode.com/users"
 let API_POST_LIST: String = "https://jsonplaceholder.typicode.com/posts?userId="
 
-struct identifier {
+struct Storyboard {
+    static let mainStoryboard = "Main"
+}
+
+struct Identifier {
+    static let userListViewController = "userListViewControllerIdentifier"
     static let userListCell = "UserListCellIdentifier"
 }
